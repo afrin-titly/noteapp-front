af38a5a7bdebafcc0bf4704b65532f494e6638dc
lm-afrin <titlee42@gmail.com>
2021-09-27 22:33:12 +0900

    added home page

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<router-view></router-view>
+  <div>
+    Home page!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/login/Login.vue'
+import Home from '../views/home/Home.vue'
 
 const routes = [
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     component: Login
   },
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
 ]
 
 const router = createRouter({
